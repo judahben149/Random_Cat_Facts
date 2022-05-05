f8427265efdf8398ede07d8a05db030982763fc7
Judah <judahben149@gmail.com>
2022-05-05 23:15:56 +0100

    ch - did some restructuring, fixed bugs, changed ap icon

--- app/src/main/java/com/judahben149/retrofitcp/view/activities/MainActivity.kt
+++ app/src/main/java/com/judahben149/retrofitcp/view/activities/MainActivity.kt
@@ -82,8 +82,10 @@ class MainActivity : AppCompatActivity() {
         val dialog = Dialog(this)
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
         dialog.setCancelable(false)
+        dialog.setCanceledOnTouchOutside(true)
         dialog.setContentView(R.layout.custom_dialog_box)
         val yesBtn = dialog.findViewById(R.id.closeInfoDialog) as Button
+
         yesBtn.setOnClickListener {
             dialog.dismiss()
         }
@@ -124,42 +126,42 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    private fun checkForInternet(context: Context): Boolean {
-
-        // register activity with the connectivity manager service
-        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-
-        // if the android version is equal to M
-        // or greater we need to use the
-        // NetworkCapabilities to check what type of
-        // network has the internet connection
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-
-            // Returns a Network object corresponding to
-            // the currently active default data network.
-            val network = connectivityManager.activeNetwork ?: return false
-
-            // Representation of the capabilities of an active network.
-            val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
-
-            return when {
-                // Indicates this network uses a Wi-Fi transport,
-                // or WiFi has network connectivity
-                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-
-                // Indicates this network uses a Cellular transport. or
-                // Cellular has network connectivity
-                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-
-                // else return false
-                else -> false
-            }
-        } else {
-            // if the android version is below M
-            @Suppress("DEPRECATION") val networkInfo =
-                connectivityManager.activeNetworkInfo ?: return false
-            @Suppress("DEPRECATION")
-            return networkInfo.isConnected
-        }
-    }
+//    private fun checkForInternet(context: Context): Boolean {
+//
+//        // register activity with the connectivity manager service
+//        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+//
+//        // if the android version is equal to M
+//        // or greater we need to use the
+//        // NetworkCapabilities to check what type of
+//        // network has the internet connection
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//
+//            // Returns a Network object corresponding to
+//            // the currently active default data network.
+//            val network = connectivityManager.activeNetwork ?: return false
+//
+//            // Representation of the capabilities of an active network.
+//            val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
+//
+//            return when {
+//                // Indicates this network uses a Wi-Fi transport,
+//                // or WiFi has network connectivity
+//                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+//
+//                // Indicates this network uses a Cellular transport. or
+//                // Cellular has network connectivity
+//                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+//
+//                // else return false
+//                else -> false
+//            }
+//        } else {
+//            // if the android version is below M
+//            @Suppress("DEPRECATION") val networkInfo =
+//                connectivityManager.activeNetworkInfo ?: return false
+//            @Suppress("DEPRECATION")
+//            return networkInfo.isConnected
+//        }
+//    }
 }
